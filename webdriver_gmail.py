cb3f1d5891fbfb828cec1201eb8c70f17f726593
markirius <markbombomb@gmail.com>
2019-11-24 13:50:38 -0300

    comments added to code

--- webdriver_gmail.py
+++ webdriver_gmail.py
@@ -8,22 +8,43 @@ from selenium import webdriver
 username = input("Username: ")
 password = getpass.getpass()
 
+"""
+set firefox as webbrowser and set gmail's as page
+"""
 browser = webdriver.Firefox()
 browser.get("http://google.com/gmail")
 browser.maximize_window()
 
+"""
+find on html the element with 'id' with name 'identifierId'
+who is username form field
+"""
 email = browser.find_element_by_id("identifierId")
 email.send_keys(username)
 
+"""
+find on html the elemento who matchs with the next button
+"""
 nextButton = browser.find_element_by_id("identifierNext")
 nextButton.click()
 
+"""
+sip of coffee
+"""
 time.sleep(3)
 
+"""
+find on html an element with name equals password
+"""
 passwd = browser.find_element_by_xpath("//*[@name='password']")
 coded = base64.b64encode(password.encode())
 cypher = base64.b64decode(coded)
 passwd.send_keys(cypher.decode())
 
+"""
+find on html element with id 'passwordNext' who matchs with
+next button on password form
+"""
 nextButtonPass = browser.find_element_by_id("passwordNext")
 nextButtonPass.click()
+
